ff89071825581fe59734b0fa1520ff4526c8db71
aritode <arian2k@gmail.com>
2017-09-22 15:08:52 +0300

    add #take_volume and #take_seats in Cargo/Passenger Carriage

--- trainapp/lib/carriage/cargo_carriage.rb
+++ trainapp/lib/carriage/cargo_carriage.rb
@@ -1,2 +1,17 @@
 class CargoCarriage < Carriage
+  attr_reader :volume
+
+  def initialize(number, volume_max)
+    @volume_max = volume_max
+    @volume = 0
+    super(number)
+  end
+
+  def take_volume(amount)
+    @volume += amount if amount <= available_volume && amount > 0
+  end
+
+  def available_volume
+    @volume_max - volume
+  end
 end
--- trainapp/lib/carriage/passenger_carriage.rb
+++ trainapp/lib/carriage/passenger_carriage.rb
@@ -1,2 +1,18 @@
 class PassengerCarriage < Carriage
+  MAX_SEATS = 56
+  attr_reader :reserved_seats
+
+  def initialize(number, total_seats = MAX_SEATS)
+    @total_seats = total_seats
+    @reserved_seats = 0
+    super(number)
+  end
+
+  def take_seat
+    @reserved_seats += 1 if reserved_seats < @total_seats
+  end
+
+  def seats_available
+    @total_seats - reserved_seats
+  end
 end
