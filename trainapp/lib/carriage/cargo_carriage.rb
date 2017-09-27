53fb5e63cb3cf2b0588ee394c3464a309cb64e05
aritode <arian2k@gmail.com>
2017-09-27 13:28:08 +0300

    refactor @volume to @volume_taken

--- trainapp/lib/carriage/cargo_carriage.rb
+++ trainapp/lib/carriage/cargo_carriage.rb
@@ -1,23 +1,23 @@
 class CargoCarriage < Carriage
   def initialize(number, volume_max)
-    @volume_max = volume_max
-    @volume = 0
-    @type = self.class
+    @volume_max   = volume_max
+    @volume_taken = 0
+    @type         = self.class
     validate!
     super(number)
   end
 
   def take_volume(amount)
-    @volume += amount if amount <= available_volume && amount > 0
+    @volume_taken += amount if amount <= available_volume && amount > 0
   end
 
   def available_volume
-    @volume_max - @volume
+    @volume_max - @volume_taken
   end
 
   def to_s
     "Carriage №: #{number}, type: #{@type}, available volume: #{available_volume}, " \
-    "filled volume: #{@volume}"
+    "filled volume: #{@volume_taken}"
   end
 
   private
